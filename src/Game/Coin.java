f3649163be242cb62cd9e4495c81479f8951c719
Talzyyy <Twana-Karim.Mohammad@city.ac.uk>
2026-02-22 15:43:32 +0000

    Coin shape and model

--- src/Game/Coin.java
+++ src/Game/Coin.java
@@ -1,4 +1,22 @@
-package Game;
+package game;
 
-public class Coin {
+import city.cs.engine.BodyImage;
+import city.cs.engine.CircleShape;
+import city.cs.engine.Shape;
+import city.cs.engine.StaticBody;
+import city.cs.engine.World;
+
+/**
+ * Coin is a StaticBody so it doesn't fall.
+ * When player touches it, it gets collected (destroyed).
+ */
+public class Coin extends StaticBody {
+
+    private static final Shape SHAPE = new CircleShape(0.4f);
+    private static final BodyImage IMAGE = new BodyImage("data/coin.png", 0.9f);
+
+    public Coin(World world) {
+        super(world, SHAPE);
+        addImage(IMAGE);
+    }
 }
