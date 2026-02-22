9b7cc825d1609cf80c98742f07910849d79d2d67
Talzyyy <Twana-Karim.Mohammad@city.ac.uk>
2026-02-22 15:42:40 +0000

    Background sprite and world.

--- src/Game/GameView.java
+++ src/Game/GameView.java
@@ -1,5 +1,38 @@
-package Game;
+package game;
 
-public class GameView {
+import city.cs.engine.UserView;
 
+import javax.swing.ImageIcon;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+/**
+ * View renders the world and overlays HUD text.
+ * - paintBackground: draws background image
+ * - paintForeground: draws changing stats like score
+ */
+public class GameView extends UserView {
+
+    private final GameWorld world;
+    private final Image background;
+
+    public GameView(GameWorld world, int width, int height) {
+        super(world, width, height);
+        this.world = world;
+
+        // Load background image from /data folder
+        background = new ImageIcon("data/background.png").getImage();
+    }
+
+    @Override
+    protected void paintBackground(Graphics2D g) {
+        // Draw background stretched to window size
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    @Override
+    protected void paintForeground(Graphics2D g) {
+        // Changing statistic (score) required by milestone 1
+        g.drawString("Score: " + world.getScore(), 15, 20);
+    }
 }
